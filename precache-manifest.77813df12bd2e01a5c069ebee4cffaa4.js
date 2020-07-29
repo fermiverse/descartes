96b2b98c30052627e4ad791677e5eb9b2f8d2318
fermi <fermiverse@gmail.com>
2020-07-29 11:39:12 +0530

    Updates

---
+++ precache-manifest.77813df12bd2e01a5c069ebee4cffaa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb94f20738157c0d3a7a101666181c61",
+    "url": "/descartes/index.html"
+  },
+  {
+    "revision": "b49670c0621b87460150",
+    "url": "/descartes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "922d9ad2a468622cbb8f",
+    "url": "/descartes/static/js/2.eb3ec3e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/descartes/static/js/2.eb3ec3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49670c0621b87460150",
+    "url": "/descartes/static/js/main.08261e49.chunk.js"
+  },
+  {
+    "revision": "715bf0adcee96bbde5b4",
+    "url": "/descartes/static/js/runtime-main.9f3e12f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/descartes/static/media/logo.5d5d9eef.svg"
+  }
+]);
